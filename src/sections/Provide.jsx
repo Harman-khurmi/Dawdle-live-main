a46c318890bcc1adb95a98b4adde47f9d975d481
Harman-khurmi <hsingh2_be21@thapar.edu>
2024-11-07 18:26:05 +0530

    provide

--- src/sections/Provide.jsx
+++ src/sections/Provide.jsx
@@ -16,27 +16,27 @@ const Provide = () => {
         <div className="flex flex-col relative z-10 text-center p-3 max-w-4xl mx-auto items-center content-center justify-center">
           {/* heading */}
           <h1 className="lg:text-5xl md:text-4xl text-3xl xs:text-2xl font-semibold text-brandSecondary mb-4 mt-8 md:mt-16 leading-9 md:leading-tight lg:leading-tight">
-            <span className='poppins'>How do we</span> <span className="font-['Libre_Baskerville'] italic text-brandPrimary">Provide</span>
+            <span className='poppins'>How do we</span> <span className="font-['Libre_Baskerville'] italic text-brandPrimary">Provide?</span>
           </h1>
           {/* cards */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 p-4 mt-6 items-center justify-center mx-auto content-center align-middle">
             {/* Square 1 */}
-            <div className="square hover-effect transition-transform duration-300 ease-in-out transform hover:scale-105 rounded-3xl h-80 xs:h-110">
+            <div className="square hover-effect transition-transform duration-300 ease-in-out transform hover:scale-105 rounded-3xl h-auto">
               <div className="content p-8 items-center flex flex-col justify-center gap-4 ">
-                <img src={Consultant} alt="" />
-                <h2 className="text-xl font-bold mb-2">Dawdle Product Consultants</h2>
-                <p className="text-gray-700">
+                <img src={Consultant} alt="" className='h-16 xs:h-12 ' />
+                <h2 className="md:text-xl text-lg  font-bold">Dawdle Product Consultants</h2>
+                <p className="text-gray-700 text-base ">
                   Leveraging a network of professionals and organizations with expertise in your field, Dawdle Product Consultants make meaningful connections that align with your product's strengths.
                 </p>
               </div>
             </div>
 
             {/* Square 2 */}
-            <div className="square hover-effect transition-transform duration-300 ease-in-out transform hover:scale-105 rounded-3xl h-80 xs:h-110">
+            <div className="square hover-effect transition-transform duration-300 ease-in-out transform hover:scale-105 rounded-3xl h-auto">
               <div className="content p-8 items-center flex flex-col justify-center gap-4 ">
-                <img src={connections} alt="" />
-                <h2 className="text-xl font-bold mb-2">Direct Enterprise Partnerships</h2>
-                <p className="text-gray-700">
+                <img src={connections} alt="" className='h-16 xs:h-12 ' />
+                <h2 className="md:text-xl text-lg  font-bold ">Direct Enterprise Partnerships</h2>
+                <p className="text-gray-700 text-base ">
                   Dawdle partners directly with enterprises actively seeking specific solutions, matching your product to clients ready to benefit from its unique capabilities.
                 </p>
               </div>
